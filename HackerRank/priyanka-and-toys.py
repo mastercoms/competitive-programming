7a4dd5c6518e9f7bc129908413862832d7595557
mastercoms <circuitsoft@tuta.io>
2017-09-28 02:12:12 -0400

    Use count only

--- HackerRank/priyanka-and-toys.py
+++ HackerRank/priyanka-and-toys.py
@@ -2,11 +2,10 @@ if __name__ == '__main__':
     count = int(raw_input())
     weights = map(int,raw_input().split())
     weights.sort()
-    purchases = count
     comparator = -1
     for weight in weights:
         if weight > comparator:
             comparator = weight + 4
         else:
-            purchases -= 1
-    print(purchases)
+            count -= 1
+    print(count)
